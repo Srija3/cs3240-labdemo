d1b8736a9c2379515130da69a9f4a07632855125
Srija3 <st2ta@virginia.edu>
2016-02-22 18:17:40 -0500

    switched to master and created a new file in the master and made some changes to the existing files

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 from helper import greeting
+
 if __name__ == "__main__":
     greeting("Hello world")
+    print("New change")
---
+++ new.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("This is in the master branch and the new.py file")
